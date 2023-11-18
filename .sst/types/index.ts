1a9c2a39306202ea71f280aaf092d2af4fa11241
Rudi Starcevic <rudi.starcevic@pm.me>
2023-11-18 18:23:45 +0800

    Adding unit tests

--- .sst/types/index.ts
+++ .sst/types/index.ts
@@ -24,12 +24,3 @@ declare module "sst/node/bucket" {
   }
 }
 
-import "sst/node/api";
-declare module "sst/node/api" {
-  export interface ApiResources {
-    "Api": {
-      url: string;
-    }
-  }
-}
-
